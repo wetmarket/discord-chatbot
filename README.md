64dd216e90ce5ee4a15cbc3c549572efed802061
wetmarket <vadimnq@gmail.com>
2025-08-06 23:30:46 +0700

    Initial commit

---
+++ README.md
@@ -0,0 +1,26 @@
+# Discord Chat Bot 🤖
+
+A simple Discord bot that sends a random meme from Reddit when a user types the command `$meme`
+
+## Features
+
+- Sends a random meme on the `$meme` command
+- Fetches memes from an external API using `requests`
+
+## Requirements
+
+Python 3.10 + 
+
+Install dependencies with:
+
+```bash
+pip install -r requirements.txt
+```
+
+Create a `.env` file and put your bot's token there:
+
+```text
+TOKEN = your_token
+```
+
+Now you're ready to go!
---
+++ bot.py
@@ -0,0 +1,28 @@
+from dotenv import load_dotenv
+import discord
+import requests
+import json
+import os
+
+load_dotenv()
+
+def get_meme():
+    response = requests.get('https://meme-api.com/gimme')
+    json_data = json.loads(response.text)
+    return json_data['url']
+
+class MyClient(discord.Client):
+    async def on_ready(self):
+        print('Logged on as {0}!'.format(self.user))
+
+    async def on_message(self, message):
+        if message.author == self.user:
+            return
+        if message.content.startswith('$meme'):
+            await message.channel.send(get_meme())
+
+intents = discord.Intents.default()
+intents.message_content = True
+
+client = MyClient(intents=intents)
+client.run(os.getenv('TOKEN'))
